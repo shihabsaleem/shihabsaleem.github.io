5c0a60abd25b44d55e8fee0d69c97f2d55ecbd32
Shihab Rahman S <shihabchikkushihab@gmail.com>
2025-12-27 11:35:28 +0530

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://shihabsaleem.github.io/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://shihabsaleem.github.io/about",
+      lastModified: new Date(),
+    },
+     {
+      url: "https://shihabsaleem.github.io/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://shihabsaleem.github.io/privacy",
+      lastModified: new Date(),
+    },
+  ];
+}
